5b856be502c9e26ea3f5f9bca4479f99731b94e1
Ryan Tung <88029861+Ryan-Tung@users.noreply.github.com>
2026-06-25 14:48:26 +0800

    drive square pid code cleanup

--- Training Robot/src/main/java/frc/robot/commands/auto/DriveSquareWithPID.java
+++ Training Robot/src/main/java/frc/robot/commands/auto/DriveSquareWithPID.java
@@ -1,23 +1,24 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.driveCommands.DriveWithPID;
 import frc.robot.commands.driveCommands.TurnWithPID;
 
 public class DriveSquareWithPID extends AutoCommand {
     
     public DriveSquareWithPID() {
-        super (
-            new SequentialCommandGroup
-            (
-                // 1. Drive the first leg of the square
+        // 1. Create the main sequential group
+        super(new SequentialCommandGroup());
+        
+        // 2. Loop 4 times to add the 4 sides of the square
+        for (int i = 0; i < 4; i++) {
+            addCommands(
+                // Drive forward
                 new DriveWithPID(300, 1, 0, 1).withTimeout(2),
                 
-                // 3. Drive the second leg of the square
+                // Turn 90 degrees
                 new TurnWithPID(0, 10, 90, 1).withTimeout(2)
-                
-            )
-        );
+            );
+        }
     }
 }
